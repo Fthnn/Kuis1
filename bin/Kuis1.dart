41bbe7758195b85277ed20dba4dba6ac422af833
Fthnn <ahmadfathan802@gmail.com>
2023-10-02 17:00:11 +0700

    Kuis 1 dart

---
+++ bin/Kuis1.dart
@@ -0,0 +1,86 @@
+// ignore_for_file: file_names
+
+void main() {
+  //tugas1
+
+  print('hello world');
+
+  //tugas2
+  print('\n');
+  String name = 'Ahmad Fathan Aqil Faiq';
+
+  print(name);
+  print(name);
+  print(name);
+
+  //tugas3
+  print('\n');
+  String firstName = 'Ahmad Fathan';
+  String lastName = 'Aqil Faiq';
+
+  print(firstName);
+  print(lastName);
+
+  //tugas4.1
+  print('\n');
+  final array1 = [1, 2, 3];
+  var array2 = [4, 5, 6];
+
+  array1[0] = 100;
+  array2[0] = 100;
+
+  print(array1);
+  print(array2);
+
+  //tugas4.2
+  print('\n');
+  int number1 = 100;
+  double number2 = 100.5;
+
+  print(number1);
+  print(number2);
+
+  //tugas5
+  print('\n');
+  num number = 10;
+  print(number);
+
+  number = 10.5;
+  print(number);
+
+  //tugas6
+  print('\n');
+  String firstName1 = 'Ahmad Fathan';
+  String lastName1 = 'Aqil Faiq';
+
+  var fullName = '$firstName1 $lastName1';
+  print(fullName);
+
+  //tugas7
+  print('\n');
+  var text = 'ini \'OPTIMAL\' \$sekali';
+  print(text);
+
+  //tugas8
+  print('\n');
+  var name1 = firstName + lastName;
+  var name2 = 'Ahmad' 'Fathan' 'Aqil' 'Faiq';
+  print(name1);
+  print(name2);
+
+  //tugas9
+  print('\n');
+  var name3 = firstName + " " + lastName;
+  var name4 = 'Ahmad' 'Fathan' 'Aqil' 'Faiq';
+  print(name3);
+  print(name4);
+
+  //tugas 10
+  print('\n');
+  var kalimatPanjang = '''
+  ini
+  saya punya
+  proyek dart
+  banyak ''';
+  print(kalimatPanjang);
+}
